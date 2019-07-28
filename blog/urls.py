911af5c42aa0d0ab89e715c65f84d335242994cd
김종혁 <hwaeen@gmail.com>
2019-07-29 00:02:07 +0900

    add post_detail template, view, url

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_list, name='post_list')
 ]
